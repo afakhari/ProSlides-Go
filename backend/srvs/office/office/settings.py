0ed80c4eeb490b9b01dbb421cd13cd6ba2b6b2a4
amin <amin.200.bidad@gmail.com>
2025-12-03 09:08:23 +0330

    Send Leaderboard to Player

--- backend/srvs/office/office/settings.py
+++ backend/srvs/office/office/settings.py
@@ -83,8 +83,15 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': [],
