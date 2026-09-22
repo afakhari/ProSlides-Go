5b37047a64e4eaedd6b762e1ab0903c1a1b961ea
Amirali Fakhari <75338617+afakhari@users.noreply.github.com>
2026-09-22 16:34:22 +0330

    feat(web): provide REST query cache

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import AppQueryProvider from "./app/providers/AppQueryProvider.tsx";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <App />
+  <AppQueryProvider>
+    <App />
+  </AppQueryProvider>
 );
 
