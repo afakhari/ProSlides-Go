a735bb21ec74ed5a9be5f016fc75f2fe6f20c2e6
Amirali Fakhari <75338617+afakhari@users.noreply.github.com>
2026-09-22 17:40:41 +0330

    refactor(web): remove legacy useLiveSession.js ownership

--- apps/web/src/hooks/useLiveSession.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { LiveSessionContext } from "../contexts/LiveSessionContext";
-
-export const useLiveSession = () => {
-  const context = useContext(LiveSessionContext);
-  if (!context) throw new Error("useLiveSession must be used within LiveSessionProvider");
-  return context;
-};
